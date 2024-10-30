8f328572b511b45c29987c6e0bece60f2f560cb5
JiangYiJia6 <116914241+JiangYiJia6@users.noreply.github.com>
2024-10-30 14:49:52 -0400

    first commit

---
+++ app/src/main/java/com/example/deliveryapp/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.example.deliveryapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.deliveryapp.databinding.ActivityRegisterBinding;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+
+}
