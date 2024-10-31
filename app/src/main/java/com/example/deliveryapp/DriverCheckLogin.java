747cdc7c567fac636aa5492e7e236c6a983f8b80
JiangYiJia6 <116914241+JiangYiJia6@users.noreply.github.com>
2024-10-31 14:24:09 -0400

    second commit

---
+++ app/src/main/java/com/example/deliveryapp/DriverCheckLogin.java
@@ -0,0 +1,23 @@
+package com.example.deliveryapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.deliveryapp_finalproject_androidmobiledev.databinding.ActivityDriverCheckLoginBinding;
+
+import com.example.deliveryapp_finalproject_androidmobiledev.R;
+
+public class DriverCheckLogin extends AppCompatActivity {
+
+
+}
--- app/src/main/java/com/example/deliveryapp/LoginActivity.java
+++ app/src/main/java/com/example/deliveryapp/LoginActivity.java
@@ -1,21 +1,43 @@
 package com.example.deliveryapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.deliveryapp_finalproject_androidmobiledev.R;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.example.deliveryapp.databinding.ActivityLoginBinding;
+//import com.example.deliveryapp.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
+
+        Button driverButton = findViewById(R.id.btnDriver);
+        Button clientButton = findViewById(R.id.btnClient);
+
+
+        driverButton.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, DriverCheckLogin.class);
+            startActivity(intent);
+        });
+
+        clientButton.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, ClientCheckLogin.class);
+            startActivity(intent);
+        });
+    }
 }
