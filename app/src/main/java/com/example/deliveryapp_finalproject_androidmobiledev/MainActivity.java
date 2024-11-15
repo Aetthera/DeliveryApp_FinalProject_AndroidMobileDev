08d9f77c9fbf309e477b3589f2ffdcd9f1c337ca
AliBahiraei <128749287+alilite@users.noreply.github.com>
2024-11-15 10:34:19 -0500

    just for test we should remove it for final parts

--- app/src/main/java/com/example/deliveryapp_finalproject_androidmobiledev/MainActivity.java
+++ app/src/main/java/com/example/deliveryapp_finalproject_androidmobiledev/MainActivity.java
@@ -1,24 +1,23 @@
 package com.example.deliveryapp_finalproject_androidmobiledev;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        button =  findViewById(R.id.btnTips);
+        // Button to navigate to TipsActivity
+       button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, TipsActivity.class);
+            startActivity(intent);
         });
     }
 }
