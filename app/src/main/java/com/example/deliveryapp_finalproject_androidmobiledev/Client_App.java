28470eb39122faef544dbbb23bfc9bfdfa603cd6
RCTSKIES <andreishtogrea@gmail.com>
2024-11-23 15:39:32 -0500

    before forgot password feature

---
+++ app/src/main/java/com/example/deliveryapp_finalproject_androidmobiledev/Client_App.java
@@ -0,0 +1,2 @@
+package com.example.deliveryapp_finalproject_androidmobiledev;public class Client_App {
+}
---
+++ app/src/main/java/com/example/deliveryapp_finalproject_androidmobiledev/Driver_Regis.java
@@ -0,0 +1,4 @@
+package com.example.deliveryapp_finalproject_androidmobiledev;
+
+public class Diriver_Regis {
+}
